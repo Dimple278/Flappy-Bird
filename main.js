64dd7c8c31a0c1964faa572ae1d1f7d1f8920010
dimple <dsaraogi2000@gmail.com>
2024-06-08 20:19:41 +0545

    add basic canvas & global variables & animation

---
+++ main.js
@@ -0,0 +1,28 @@
+const canvas = document.getElementById('canvas1')
+const context = canvas.getContext('2d')
+canvas.width = 600;
+canvas.height = 400;
+
+let spacePressed = false;
+let angle = 0;
+let hue = 0;
+let frame = 0;
+let score = 0;
+let gameSpeed = 2;
+
+
+function animate(){
+    context.clearRect(0,0,canvas.width,canvas.height)
+    context.fillRect(10,canvas.height-90,50,50)
+    requestAnimationFrame(animate);
+}
+
+animate();
+
+window.addEventListener('keydown',(e)=>{
+    if(e.code == 'Space')spacePressed= true;
+})
+
+window.addEventListener('keyup',(e)=>{
+    if(e.code == 'Space') spacePressed = false;
+})
